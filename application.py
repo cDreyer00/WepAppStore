f60ec945cda66db3bd8018d75fa97dbe85fabdb7
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2022-10-08 20:56:43 -0300

    initializing page

--- application.py
+++ application.py
@@ -1,2 +1,14 @@
 from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+import sqlite3
 
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+db = sqlite3.connect("database")
+
+@app.route("/")
+def index():
+    return "PAGE"
